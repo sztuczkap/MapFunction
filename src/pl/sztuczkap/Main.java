7a759e89ba4802741f6be7fd41def073f6990266
sztuczkap <p.sztuczka@wp.pl>
2021-01-05 20:40:24 +0100

    Metoda map

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,34 @@
 package pl.sztuczkap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student(
+                "Wojtek",
+                23874L,
+                List.of("Inżynieria oprogramowania", "Programownaie obiektowe", "Matematyka dyskretna")
+        ));
+        studentList.add(new Student(
+                "Anna",
+                63883L,
+                List.of("Psychologia społeczna", "Terapia dzieci", "Antropologia")
+        ));
+        studentList.add(new Student(
+                "Ola",
+                736363L,
+                List.of("Automatyka", "Matematyka dyskretna", "Mechanika")
+        ));
+
+//        studentList.stream().map(Student::getName).forEach(System.out::println);
+
+        studentList
+                .stream()
+                .flatMap(student -> student.getLectures().stream()).forEach(System.out::println);
+        // flat map spłaszcza strukture obiektów
     }
 }
